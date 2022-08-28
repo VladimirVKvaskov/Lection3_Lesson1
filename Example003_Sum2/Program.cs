5a39403dba856013501b02b8ab956afebc86dbd4
unknown <vladimir_kvaskov@mail.ru>
2022-08-28 15:27:29 +0300

    добавлен пример сложениz 2 чисел

--- Example003_Sum2/Program.cs
+++ Example003_Sum2/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA = 31;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(numberA + numberB);
+
+
