2e63566e8e92aa018adb4b94d2fc695bdd479280
unknown <vladimir_kvaskov@mail.ru>
2022-08-30 16:45:04 +0300

    Решили упражнени е по нахождению четных чисел

--- Example011_Task6_sum_btwn/Program.cs
+++ Example011_Task6_sum_btwn/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+int a = 8;
+int count = 1;
+
+    while(count < a+1)
+
+    {   
+        if (count % 2 == 0)
+        Console.WriteLine(count);
+        count = count + 1;
+    }
