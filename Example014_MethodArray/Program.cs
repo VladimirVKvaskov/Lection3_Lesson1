090de2c1f24872f6ad3da581edd5b85f3a1dcf13
VladimirVKvaskov <vladimir_kvaskov@mail.com>
2022-09-03 15:35:28 +0300

    create arrayLibrary

--- Example014_MethodArray/Program.cs
+++ Example014_MethodArray/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+int[] array = { 11, 211, 31, 4, 15, 61, 17, 18 };
+
+
+int n = array.Lenght;
+
+
+/* int find = 4;
+ 
+int index = 0;
+
+while (index < n)
+
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    //index = index +1;
+    index++;
+}
+
+*/
+
+//Console.WriteLine(n);
+
+ 
