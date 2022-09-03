ec148ae667e4b6516d5ca1bf964fed38f2e48825
VladimirVKvaskov <vladimir_kvaskov@mail.com>
2022-09-03 19:43:20 +0300

    arrayLibrary ready

--- Example015_arrayLibrary/Program.cs
+++ Example015_arrayLibrary/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+void FillArray(int[] collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+
+        collection[index] = new Random().Next(1, 10);
+        //index = index + 1;
+        index++;
+    }
+}
+
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        //position = position + 1;
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        //index = index + 1;
+        index++;
+    }
+    return position;
+
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+array[4] = 13;
+array [6] = 13;
+
+PrintArray(array);
+
+Console.WriteLine();
+
+int pos = IndexOf(array,444);
+Console.WriteLine(pos);
+
