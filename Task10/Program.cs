be92e0e7a3305f187ae8e9621852d88383d716e2
VladimirVKvaskov <vladimir_kvaskov@mail.com>
2022-09-03 21:21:34 +0300

    task13ready

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -3,4 +3,5 @@
 Console.Write("Введите трехзначное число: ");
 int threeDigit = Convert.ToInt32(Console.ReadLine());
 string digitNumber = Convert.ToString(threeDigit);
+
 Console.WriteLine(digitNumber[1]);
--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int abc = Convert.ToInt32(Console.ReadLine());
+string abcString = Convert.ToString(abc);
+
+    if (abcString.Length > 2)
+        {
+      Console.WriteLine("Третья цифра = " + abcString[2]);
+        }
+    else 
+        {
+      Console.WriteLine("третьей цифры нет");
+        }
