40dea1980f9a118d07eab6d55283b188c4de1a25
VladimirVKvaskov <vladimir_kvaskov@mail.com>
2022-09-03 22:00:09 +0300

    Task13ready

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите номер дня недели от 1 до 7: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+DayWeek(dayNumber);
+
+void DayWeek (int dayNumber) 
+    {
+        if (dayNumber == 6 || dayNumber == 7) {
+        Console.WriteLine("выходной");
+            }
+                else if (dayNumber < 1 || dayNumber > 7) {
+                 Console.WriteLine("в неделе от 1 до 7 дней");
+             }
+                 else Console.WriteLine("будний");
+    }
+
