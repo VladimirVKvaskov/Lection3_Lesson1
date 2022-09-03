ad5d1506e5939a14e0d1c12ecbd4f89734e035f7
VladimirVKvaskov <vladimir_kvaskov@mail.com>
2022-09-03 16:16:04 +0300

    method array ready

--- Example014_MethodArray/Program.cs
+++ Example014_MethodArray/Program.cs
@@ -1,12 +1,13 @@
 ﻿Console.Clear();
 
+//               0   1    2  3   4   5   6   7
 int[] array = { 11, 211, 31, 4, 15, 61, 17, 18 };
 
 
-int n = array.Lenght;
+int n = array.Length;
 
 
-/* int find = 4;
+int find = 211;
  
 int index = 0;
 
@@ -16,12 +17,12 @@ while (index < n)
     if (array[index] == find)
     {
         Console.WriteLine(index);
+        break;
     }
     //index = index +1;
     index++;
 }
 
-*/
 
 //Console.WriteLine(n);
 
